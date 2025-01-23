287cc846e4a780d6b09021217db3c145ce1ba2c7
SABBIDO7 <sabbidinewalid12345@gmail.com>
2025-01-23 22:11:49 +0100

    reducing snackbar showing time

--- frontend/src/components/SnackbarAlert.tsx
+++ frontend/src/components/SnackbarAlert.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Snackbar, Alert } from '@mui/material';
+import React from 'react'
+import { Snackbar, Alert } from '@mui/material'
 
 interface SnackbarAlertProps {
-  open: boolean;
-  message: string;
-  severity: 'success' | 'error';
-  onClose: (event: React.SyntheticEvent | Event, reason?: string) => void;
+  open: boolean
+  message: string
+  severity: 'success' | 'error'
+  onClose: (event: React.SyntheticEvent | Event, reason?: string) => void
 }
 
 function SnackbarAlert({
@@ -15,12 +15,12 @@ function SnackbarAlert({
   onClose,
 }: SnackbarAlertProps) {
   return (
-    <Snackbar open={open} autoHideDuration={6000} onClose={onClose}>
+    <Snackbar open={open} autoHideDuration={3000} onClose={onClose}>
       <Alert onClose={onClose} severity={severity} sx={{ width: '100%' }}>
         {message}
       </Alert>
     </Snackbar>
-  );
+  )
 }
 
-export default SnackbarAlert;
+export default SnackbarAlert
